473324e49679f4a6b81b781b3eba4908a3155bfb
Riki Ruswandi <riki@pratamatechsolution.co.id>
2025-05-28 09:56:36 +0700

    Fix API GET

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,6 +1,9 @@
 // src/routes/example/+page.ts
+import { PUBLIC_GET_ALL_DINOSAURS, PUBLIC_RESTASAURUS_URL } from '$env/static/public';
+
 export async function load({ fetch }) {
-    const res = await fetch('http://localhost:3000/api/v1/dinosaurs');
+    const fullUrl = `${PUBLIC_RESTASAURUS_URL}${PUBLIC_GET_ALL_DINOSAURS}`;
+    const res = await fetch(fullUrl);
     const data = await res.json();
 
     return { data };
