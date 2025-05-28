3bc4284498e98b9a6627bd2ba451b161db937470
Riki Ruswandi <riki@pratamatechsolution.co.id>
2025-05-28 10:04:56 +0700

    Statistic Card

--- src/lib/components/molecules/StatisticCard/StatisticCard.svelte
+++ src/lib/components/molecules/StatisticCard/StatisticCard.svelte
@@ -1,14 +1,14 @@
 <div class="mb-10 grid grid-cols-1 gap-6 md:grid-cols-3">
-    <div class="rounded-2xl bg-blue-50 p-6 text-center shadow">
+    <div class="rounded bg-blue-50 dark:bg-white/20 dark:backdrop-blur-sm p-6 text-center shadow">
         <h3 class="text-4xl font-bold text-blue-600">147</h3>
-        <p class="text-gray-600">Total Clade Unik</p>
+        <p class="text-gray-600 dark:text-teal-100">Total Clade Unik</p>
     </div>
-    <div class="rounded-2xl bg-green-50 p-6 text-center shadow">
+    <div class="rounded bg-green-50 dark:bg-white/20 dark:backdrop-blur-sm p-6 text-center shadow">
         <h3 class="text-4xl font-bold text-green-600">3562</h3>
-        <p class="text-gray-600">Total Spesies Tercatat</p>
+        <p class="text-gray-600 dark:text-teal-100">Total Spesies Tercatat</p>
     </div>
-    <div class="rounded-2xl bg-yellow-50 p-6 text-center shadow">
+    <div class="rounded bg-yellow-50 dark:bg-white/20 dark:backdrop-blur-sm p-6 text-center shadow">
         <h3 class="text-4xl font-bold text-yellow-600">Dinosauria</h3>
-        <p class="text-gray-600">Clade Terbesar</p>
+        <p class="text-gray-600 dark:text-teal-100">Clade Terbesar</p>
     </div>
 </div>
