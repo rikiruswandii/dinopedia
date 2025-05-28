8ad28cdbf9a90805f20fd7ea497e545b73a2ff44
Riki Ruswandi <riki@pratamatechsolution.co.id>
2025-05-28 10:04:01 +0700

    Dinosaur Card

--- src/lib/components/organisms/DinosaurCard/DinosaurCard.svelte
+++ src/lib/components/organisms/DinosaurCard/DinosaurCard.svelte
@@ -1,22 +1,24 @@
 <script lang="ts">
-	import { DinosaurClasification, DinosaurImage, DinosaurSource } from "$lib/components/molecules";
-    export let dinosaur;
+	import { DinosaurClasification, DinosaurImage, DinosaurSource } from '$lib/components/molecules';
+	export let dinosaur;
 </script>
 
-<div class="bg-white rounded-lg shadow-lg p-6 mb-8 max-w-4xl mx-auto">
-    <h2 class="text-2xl font-bold mb-4">{dinosaur.name}</h2>
-    <p class="mb-3 italic text-gray-600">{dinosaur.temporalRange}</p>
-    <p class="mb-4">{dinosaur.description}</p>
-  
-    <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
+<div class="mx-auto mb-8 max-w-4xl rounded bg-white dark:bg-white/20 dark:backdrop-blur-sm p-6 shadow-sm">
+	<h2 class="mb-4 text-2xl font-bold">{dinosaur.name}</h2>
+	<p class="mb-3 text-gray-600 italic">{dinosaur.temporalRange}</p>
+	<p class="mb-4">{dinosaur.description}</p>
+
+	<div class="grid grid-cols-1 md:grid-cols-[auto_1fr] gap-6">
+    <div class="overflow-x-auto">
       <DinosaurImage image={dinosaur.image} />
-      <div>
-        <p><strong>Diet:</strong> {dinosaur.diet}</p>
-        <p><strong>Lokomosi:</strong> {dinosaur.locomotionType}</p>
-  
-        <DinosaurClasification classification={dinosaur.classificationInfo} />
-      </div>
     </div>
-  
-    <DinosaurSource source={dinosaur.source} />
+    <div>
+      <p><strong>Diet:</strong> {dinosaur.diet}</p>
+      <p><strong>Lokomosi:</strong> {dinosaur.locomotionType}</p>
+      <DinosaurClasification classification={dinosaur.classificationInfo} />
+    </div>
   </div>
+  
+
+	<DinosaurSource source={dinosaur.source} />
+</div>
