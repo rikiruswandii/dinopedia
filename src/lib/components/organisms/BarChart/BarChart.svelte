a6483d3b66ee6ccd662bd929b6e610d9c187441a
Riki Ruswandi <riki@pratamatechsolution.co.id>
2025-05-28 10:04:24 +0700

    Bar Chart

--- src/lib/components/organisms/BarChart/BarChart.svelte
+++ src/lib/components/organisms/BarChart/BarChart.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
     let { children } = $props();
 </script>
-<div class="rounded-2xl bg-gray-100 p-6 shadow">
-    <h3 class="mb-4 text-xl font-semibold text-gray-700">
+<div class="rounded bg-gray-100 dark:bg-white/20 dark:backdrop-blur-sm p-6 shadow">
+    <h3 class="mb-4 text-xl font-semibold text-gray-700 dark:text-teal-200 dark:underline dark:decoration-yellow-200">
         Top 10 Clade Berdasarkan Jumlah Spesies
     </h3>
     <!-- Chart component will be mounted here -->
