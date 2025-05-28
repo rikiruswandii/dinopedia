3371d64505a936ca4f9400bc72112d339d79b028
Riki Ruswandi <riki@pratamatechsolution.co.id>
2025-05-28 11:52:18 +0700

    Set interface DinosaurImage

--- src/lib/components/molecules/DinosaurImage/DinosaurImage.svelte
+++ src/lib/components/molecules/DinosaurImage/DinosaurImage.svelte
@@ -1,12 +1,30 @@
-<script>
+<script lang="ts">
 	import { onMount } from 'svelte';
+	interface DinosaurImage {
+		title: string;
+		description: string;
+		imageURL: string;
+		authorURL: string;
+		author: string;
+		licenseURL: string;
+		license: string;
+	}
 
-	export let image = {};
+	// DinosaurList.svelte
+	export let image: DinosaurImage = {
+		title: '',
+		description: '',
+		imageURL: '',
+		authorURL: '',
+		author: '',
+		licenseURL: '',
+		license: ''
+	};
 
 	let directImageURL = '';
 
 	// Ambil URL langsung dari Wikimedia API
-	async function getDirectImageURL(filename) {
+	async function getDirectImageURL(filename: string) {
 		const title = filename.replace('https://commons.wikimedia.org/wiki/File:', '');
 		const apiURL = `https://commons.wikimedia.org/w/api.php?action=query&titles=File:${encodeURIComponent(title)}&prop=imageinfo&iiprop=url&format=json&origin=*`;
 
@@ -14,7 +32,7 @@
 		const data = await res.json();
 
 		const pages = data.query.pages;
-		const page = Object.values(pages)[0];
+		const page:any = Object.values(pages)[0];
 
 		if (page.imageinfo && page.imageinfo[0].url) {
 			directImageURL = page.imageinfo[0].url;
@@ -32,12 +50,14 @@
 	{#if directImageURL}
 		<img src={directImageURL} alt={image.title} class="h-64 w-full object-contain" />
 	{:else}
-		<div class="h-64 w-full flex items-center justify-center bg-gray-200 text-gray-500 text-sm">Memuat gambar...</div>
+		<div class="flex h-64 w-full items-center justify-center bg-gray-200 text-sm text-gray-500">
+			Memuat gambar...
+		</div>
 	{/if}
 
 	<div class="bg-gray-100 px-4 py-2">
 		<h3 class="font-semibold dark:text-gray-700">{image.title}</h3>
-		<p class="text-sm text-gray-600">{image.description}</p>
+		<p class="text-sm text-gray-600">{@html image.description}</p>
 		<p class="mt-2 text-xs dark:text-gray-600">
 			By
 			<a href={image.authorURL} target="_blank" class="text-teal-500 hover:underline">
