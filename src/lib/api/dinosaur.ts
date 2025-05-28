a3a1e7b0f507e465804f1d78cfb8b697bb9dfb41
Riki Ruswandi <riki@pratamatechsolution.co.id>
2025-05-28 10:01:49 +0700

    Function for load more data from API

---
+++ src/lib/api/dinosaur.ts
@@ -0,0 +1,14 @@
+// src/routes/dinosaurs/+page.ts
+import { PUBLIC_RESTASAURUS_URL } from '$env/static/public';
+
+export async function loadMoreDinos(endpoint: string) {
+    const fullUrl = `${PUBLIC_RESTASAURUS_URL}${endpoint}`;
+
+    const res = await fetch(fullUrl);
+    if (!res.ok) {
+        throw new Error(`Gagal fetch: ${res.status}`);
+    }
+
+    return await res.json();
+}
+
