ba03d34396c242f464d56b590ee7d09b828cf9b3
Riki Ruswandi <riki@pratamatechsolution.co.id>
2025-06-03 08:35:55 +0700

    Make component organism (Footer)

---
+++ src/lib/components/organisms/Footer/Footer.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import { m } from '$lib/paraglide/messages.js';
+    import { PUBLIC_APP_NAME } from '$env/static/public';
+	const appName: string = PUBLIC_APP_NAME || 'Dinopedia';
+</script>
+
+<footer class="bg-background dark:bg-background py-6 text-center text-sm text-gray-700 dark:text-gray-300">
+	{m.footer_text({ name: appName })}
+</footer>
