df7b8102b3383193673a9b1f5870cec0b3c52bd9
Riki Ruswandi <riki@pratamatechsolution.co.id>
2025-06-03 08:32:15 +0700

    Refaktor display data from API

--- src/lib/components/organisms/BarChart/BarChart.svelte
+++ src/lib/components/organisms/BarChart/BarChart.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
+    import { m } from '$lib/paraglide/messages.js';
     let { children } = $props();
 </script>
 <div class="rounded bg-gray-100 dark:bg-white/20 dark:backdrop-blur-sm p-6 shadow">
     <h3 class="mb-4 text-xl font-semibold text-gray-700 dark:text-teal-200 dark:underline dark:decoration-yellow-200">
-        Top 10 Clade Berdasarkan Jumlah Spesies
+       {m.species_distribution()}
     </h3>
     <!-- Chart component will be mounted here -->
     {@render children()}
