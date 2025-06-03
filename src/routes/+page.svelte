86da9ad9c9a18e80037f7d14b5e60812607f6d0d
Riki Ruswandi <riki@pratamatechsolution.co.id>
2025-06-03 09:04:22 +0700

    Implement Header organism in page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,7 @@
 	import { Label } from '$lib/components/ui/label';
 	import { mode, setMode } from 'mode-watcher';
 	import { onMount } from 'svelte';
-	import { BarChart, DinosaurList, Chart, Footer } from '$lib/components/organisms';
+	import { BarChart, DinosaurList, Chart, Footer, Header } from '$lib/components/organisms';
 	import { StatisticCard } from '$lib/components/molecules';
 	import { Skeleton } from '$lib/components/ui/skeleton/index.js';
 	import { m } from '$lib/paraglide/messages.js';
@@ -40,130 +40,7 @@
 </script>
 
 <section class="bg-background dark:bg-background">
-	<header class="absolute inset-x-0 top-0 z-50">
-		<nav class="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
-			<div class="flex lg:flex-1">
-				<a href="/" class="-m-1.5 p-1.5 text-gray-700 dark:text-gray-200">
-					<span class="sr-only">{appName.toLocaleUpperCase}</span>
-					<img
-						class="h-8 w-auto"
-						src="https://cdn-icons-png.flaticon.com/128/472/472751.png"
-						alt=""
-					/>
-				</a>
-			</div>
-			<div class="flex lg:hidden">
-				<button
-					type="button"
-					class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700 dark:text-gray-200"
-				>
-					<span class="sr-only">Open main menu</span>
-					<svg
-						class="size-6"
-						fill="none"
-						viewBox="0 0 24 24"
-						stroke-width="1.5"
-						stroke="currentColor"
-						aria-hidden="true"
-						data-slot="icon"
-					>
-						<path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-						/>
-					</svg>
-				</button>
-			</div>
-			<div class="hidden lg:flex lg:gap-x-12">
-				<a href="/" class="text-sm/6 font-semibold text-gray-900 dark:text-teal-200"
-					>{m.nav_home()}</a
-				>
-				<a href="/" class="text-sm/6 font-semibold text-gray-900 dark:text-teal-200">{m.dino()}</a>
-				<a href="/" class="text-sm/6 font-semibold text-gray-900 dark:text-teal-200"
-					>{m.nav_explorer()}</a
-				>
-				<a href="/" class="text-sm/6 font-semibold text-gray-900 dark:text-teal-200"
-					>{m.nav_about()}</a
-				>
-			</div>
-			<div class="hidden lg:flex lg:flex-1 lg:justify-end">
-				<div class="flex items-center space-x-2">
-					<Switch bind:checked={darkMode} id="dark-mode" />
-					<Label for="dark-mode" class="font-normal text-gray-900 dark:text-teal-200">
-						{darkMode ? m.dark_mode() : m.light_mode()}
-					</Label>
-				</div>
-			</div>
-		</nav>
-		<!-- Mobile menu, show/hide based on menu open state. -->
-		<div class="lg:hidden" role="dialog" aria-modal="true">
-			<!-- Background backdrop, show/hide based on slide-over state. -->
-			<div class="fixed inset-0 z-50"></div>
-			<div
-				class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10 dark:bg-black"
-			>
-				<div class="flex items-center justify-between">
-					<a href="/" class="-m-1.5 p-1.5">
-						<span class="sr-only">{appName}</span>
-						<img
-							class="h-8 w-auto"
-							src="https://cdn-icons-png.flaticon.com/128/472/472751.png"
-							alt=""
-						/>
-					</a>
-					<button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700 dark:text-gray-400">
-						<span class="sr-only">Close menu</span>
-						<svg
-							class="size-6"
-							fill="none"
-							viewBox="0 0 24 24"
-							stroke-width="1.5"
-							stroke="currentColor"
-							aria-hidden="true"
-							data-slot="icon"
-						>
-							<path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
-						</svg>
-					</button>
-				</div>
-				<div class="mt-6 flow-root">
-					<div class="-my-6 divide-y divide-gray-500/10 dark:divide-gray-400/10">
-						<div class="space-y-2 py-6">
-							<a
-								href="/"
-								class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50 dark:text-gray-300"
-								>{m.nav_home()}</a
-							>
-							<a
-								href="/"
-								class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50 dark:text-gray-300"
-								>{m.dino()}</a
-							>
-							<a
-								href="/"
-								class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50 dark:text-gray-300"
-								>{m.nav_explorer()}</a
-							>
-							<a
-								href="/"
-								class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50 dark:text-gray-300"
-								>{m.nav_about()}</a
-							>
-						</div>
-						<div class="py-6">
-							<div class="flex items-center space-x-2">
-								<Switch bind:checked={darkMode} id="dark-mode" />
-								<Label for="dark-mode" class="font-normal text-gray-900 dark:text-teal-200">
-									{darkMode ? m.dark_mode() : m.light_mode()}
-								</Label>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</header>
+	<Header />
 
 	<div class="relative isolate px-6 pt-14 lg:px-8">
 		<div
