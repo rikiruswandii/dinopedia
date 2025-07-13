d7a84009bdf44e17dd2720b56f873c1733b08e40
rikiruswandii <rikiruswandi28@gmail.com>
2025-07-13 19:13:01 +0700

    feat(modified):images data

--- src/lib/data/images.ts
+++ src/lib/data/images.ts
@@ -7,10 +7,11 @@ const Triceratops = makeImage('https://i.pinimg.com/736x/f3/0a/51/f30a51245c64e5
 const TyrannosaurusRex = makeImage('https://i.pinimg.com/736x/f7/1e/59/f71e59d276576943b8451e1117ef0539.jpg', 'Dino Tyrannosaurus Rex', 'Dino Tyrannosaurus Rex');
 const Velociraptor = makeImage('https://i.pinimg.com/736x/3a/17/30/3a173011e4eb5a5d2693b30dc58654f0.jpg', 'Dino Velociraptor', 'Dino Velociraptor');
 const Stegosaurus = makeImage('https://i.pinimg.com/736x/74/25/fb/7425fb50fb0b0f0e215471594041986d.jpg', 'Dino Stegosaurus', 'Dino Stegosaurus');
-const Shizuka = makeImage('https://awesomepng.com/thumbnail/broccoli/1-green-broccoli-png-image-thumb.png', 'Dino Shizuka', 'Dino Shizuka');
-const Suneo = makeImage('https://awesomepng.com/thumbnail/cartoon/92422--free-clipart-hd-thumb.png', 'Suneo', 'Suneo');
-const Nobita = makeImage('https://awesomepng.com/thumbnail/artwork/93004-shizuka-nobi-yellow-minamoto-sewashi-facial-expression-thumb.png', 'Nobita', 'Nobita');
-const Doraemon = makeImage('https://awesomepng.com/thumbnail/nose/90613-art-human-nobi-doraemon-behavior-dorami-nobita-thumb.png', 'Doraemon', 'Doraemon');
-const Giant = makeImage('https://awesomepng.com/thumbnail/doraemon/71299-nobi-youtube-nobita-television-doraemon-free-photo-png-thumb.png', 'Giant', 'Giant');
+const Diets = makeImage('https://i.pinimg.com/1200x/77/23/cd/7723cda1608da0a60f25bd4ccaff456b.jpg', 'Dino Diets', 'Dino Diets');
+const Locomotions = makeImage('https://i.pinimg.com/1200x/88/90/74/889074484318e23d0f355b303a14c476.jpg', 'Locomotions', 'Locomotions');
+const Dinosaurs = makeImage('https://i.pinimg.com/736x/15/cc/09/15cc09a608484fa57ee97725655bbfdc.jpg', 'Dinosaurs', 'Dinosaurs');
+const DinosaurImages = makeImage('https://i.pinimg.com/736x/80/dc/b3/80dcb370c93fa9fba5e73ce72fda94d9.jpg', 'DinosaurImages', 'DinosaurImages');
+const Clades = makeImage('https://i.pinimg.com/736x/4a/2e/cd/4a2ecd13b70d87b05e510df90b1bd16d.jpg', 'Clades', 'Clades');
+const bdDinosaurs = makeImage('https://i.pinimg.com/736x/3e/3f/3d/3e3f3d6e3335124d379894cac0b66f95.jpg', 'bdDinosaurs', 'bdDinosaurs');
 
-export { dinoMeme, Triceratops, TyrannosaurusRex, Velociraptor, Stegosaurus, Shizuka, Suneo, Nobita, Doraemon, Giant }
+export { dinoMeme, Triceratops, TyrannosaurusRex, Velociraptor, Stegosaurus, Diets, Locomotions, Dinosaurs, DinosaurImages, Clades, bdDinosaurs }
