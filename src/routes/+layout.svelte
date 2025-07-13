3fbadc68e5112c35fa8f9513f18ba8463f37cb94
rikiruswandii <rikiruswandi28@gmail.com>
2025-07-13 19:12:11 +0700

    feat(modified):layout

--- src/routes/+layout.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-	import '../app.css';
-	import { ModeWatcher } from "mode-watcher";
-	let { children } = $props();
-</script>
-<ModeWatcher defaultMode={"light"} themeColors={{ dark: "black", light: "white" }} />
-{@render children()}
